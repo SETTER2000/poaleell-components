5e4ab98ea28e8a17c126d54deceec340b45e86bd
SETTER <info@lphp.ru>
2019-05-13 18:32:34 +0300

    Ed

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
     <div id="app">
         <img alt="Vue logo" src="./assets/logo.png">
         <HelloWorld msg="Welcome to Your Vue.js App"/>
-        <TablePoaleell />
+        <TablePoaleell p-class="table table-striped"/>
     </div>
 </template>
 
